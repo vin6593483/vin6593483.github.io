b6bb5f21c5882202d75ef36f54465ecb137f38fe
vin6593483 <vin6593483@gmail.com>
2026-02-28 21:59:27 +0800

    Add files via upload

---
+++ 大一上/HW13-1 Linked List 加入節點/HW13-1 Linked List 加入節點.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+struct node {
+	int n;
+	node* next;
+};
+int main(void) {
+	node* first = (node*)malloc(sizeof(node));
+	first->n = 1;
+	first->next = NULL;
+	node* p = first;
+	int num;
+	while (cin >> num && 0 < num && num < 1000) {
+		node* input = (node*)malloc(sizeof(node));
+		p->next = input;
+		input->n = num;
+		input->next = NULL;
+		p = input;
+	}
+	if (num == 0) {
+		for (node* c = first; c != NULL; c = c->next) {
+			cout << c->n << "->";
+		}
+	}
+	cout << "NULL";
+	return 0;
+}
