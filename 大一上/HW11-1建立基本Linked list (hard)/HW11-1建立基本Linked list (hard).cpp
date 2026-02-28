6d60f40c9c051e692e0a77b007983f292120a796
vin6593483 <vin6593483@gmail.com>
2026-02-28 21:52:36 +0800

    Add files via upload

---
+++ 大一上/HW11-1建立基本Linked list (hard)/HW11-1建立基本Linked list (hard).cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+struct node {
+	int number;
+	node* next_struct;
+};
+int main(void) {
+	node info[6];
+	for (int i = 0; i < 6; i++) {
+		cin >> info[i].number;
+		info[i].next_struct = &info[i + 1];
+	}
+	info[5].next_struct = NULL;
+	for (int i = 0; i < 6; i++) {
+		cout << info[i].number;
+		cout << '->';
+	}
+	cout << "NULL";
+	return 0;
+}
