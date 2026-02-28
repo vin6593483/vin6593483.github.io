552f74ebb3b69c6523a3bbd68c07230dbe2f63fe
vin6593483 <vin6593483@gmail.com>
2026-02-28 21:50:56 +0800

    Add files via upload

---
+++ 大一上/HW10-4 氣泡排序排列/HW10-4 氣泡排序排列.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<cstdlib>
+using namespace std;
+void sort(int* arr,int n) {
+	for (int i = 0; i < n-1 ; i++) {
+		for (int j = 0; j < n - 1 - i; j++) {
+			if (arr[j] > arr[j + 1]) {
+				int temp = arr[j];
+				arr[j] = arr[j + 1];
+				arr[j + 1] = temp;
+			}
+		}
+	}
+}
+int main(void) {
+	int n;
+	cin >> n;
+	int* arr;
+	arr = (int*)malloc(n*sizeof(int));
+	for (int i = 0; i < n; i++) {
+		cin >> arr[i];
+	}
+	sort(arr,n);
+	for (int i = 0; i < n; i++) {
+		cout << arr[i] << " ";
+	}
+	return 0;
+}
